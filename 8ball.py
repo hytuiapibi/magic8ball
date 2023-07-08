7ea0422459780e8e287b14e625f4b94ce720e482
Anthony Batitto <ebatitto@gmail.com>
2023-07-08 00:43:16 -0400

    Update 8ball.py

--- 8ball.py
+++ 8ball.py
@@ -8,29 +8,29 @@ random_number = random.randint(1,12)
 #print(random_number)
 
 if random_number == 1:
-  answer = "Yes - definitely"
+  answer = "Sure whatever."
 elif random_number == 2:
-  answer = "It is decidedly so"
+  answer = "Who asks this kind of question?"
 elif random_number == 3:
-  answer = "Without a doubt"
+  answer = "Darn tootin'"
 elif random_number == 4:
-  answer = "Reply hazy, try again"
+  answer = "Your question cannot be answered at this time. Please hang up and try again."
 elif random_number == 5:
-  answer = "Ask again later"
+  answer = "Better believe it pal."
 elif random_number == 6:
-  answer = "Better not tell you now"
+  answer = "Fuggetaboutit."
 elif random_number == 7:
-  answer = "My sources say no"
+  answer = "Bury your hopes on that one."
 elif random_number == 8:
-  answer = "Outlook not so good"
+  answer = "Negative."
 elif random_number == 9:
-  answer = "Very doubtful"
+  answer = "In this economy?"
 elif random_number == 10:
-  answer = "Please stop asking questions"
+  answer = "Yeah."
 elif random_number == 11:
-  answer = "Ask something else"
+  answer = "I do not understand the statement."
 elif random_number == 12:
-  answer = "I guess"
+  answer = "I guess."
 else:
   answer = "Error"
 
